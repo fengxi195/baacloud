37a016fdf9af95c9403a6482520b910ae95c859a
fengxi <43842590+fengxi195@users.noreply.github.com>
2019-04-07 20:19:48 +0800

    Update baiduyun_captcha.py

--- baiduyun_captcha.py
+++ baiduyun_captcha.py
@@ -2,11 +2,11 @@ import requests as req
 import base64
 
 def captche_main(image_data):
-    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=Rjx1dnlPVhrGzpeztHhvjnHC&client_secret=mQCq7zsrkThAqFhrvUMINBdNkuRDslbG'
+    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=【官网获取的AK】&client_secret=【官网获取的SK】'
     header = {
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    res = req.post(host)
+    res = req.post(host) #获取百度access_token
     access_token = res.json()['access_token']
     temp_url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=' + access_token
     temp_data = {
